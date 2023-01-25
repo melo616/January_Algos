222ea845f35d9f99a11d3fc50e6f545876682ca9
Kat Jarboe <jarboek1@gmail.com>
2023-01-25 10:03:56 -0600

    Added findObjects and insert

---
+++ findObjects.js
@@ -0,0 +1,71 @@
+/* 
+  Given a search criteria object, whose values will only be
+  primitives (ints, strings, booleans) and a list of objects.
+  return any object that matches all the key value pairs in the search
+  criteria object.
+  
+  Bonus: write a 2nd solution using built in methods to practice functional
+  programming.
+*/
+
+const items = [
+    { firstName: "Bob", lastName: "Bobbert", age: 31 },
+    { firstName: "John", lastName: "Smith", age: 25 },
+    { firstName: "Bob", lastName: "Smith", age: 27 },
+    { firstName: "Bob", lastName: "White", age: 31 },
+  ];
+  
+  const searchCriteria1 = {
+    firstName: "Bob",
+    age: 31,
+  };
+  const expected1 = [
+    { firstName: "Bob", lastName: "Bobbert", age: 31 },
+    { firstName: "Bob", lastName: "White", age: 31 },
+  ];
+  
+  const searchCriteria2 = {
+    lastName: "Smith",
+  };
+  const expected2 = [
+    { firstName: "John", lastName: "Smith", age: 25 },
+    { firstName: "Bob", lastName: "Smith", age: 27 },
+  ];
+  
+  /**
+   * Finds the objects that match the given search criteria.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Object} criteria
+   * @param {Array<Object>} collection
+   * @returns {Array<Object>} The found objects.
+   */
+function findObjects(criteria, collection) {
+    const results = [];
+    for (let i=0; i<collection.length; i++) {
+        let isMatch = true;
+        for (key in criteria) {
+            if (collection[i][key] !== criteria[key]) {
+                // console.log(collection[i][key] + "=" + criteria[key])
+                isMatch = false;
+            }
+        } 
+        if (isMatch === true) {
+            results.push(collection[i])
+        }
+    }
+    return results;
+}
+
+console.log(findObjects(searchCriteria1, items))
+console.log(findObjects(searchCriteria2, items))
+/**
+   * - Time: O(?).
+   * - Space: O(?).
+   */
+
+function findObjectsFunctional(criteria, collection) {
+    
+}
+
+/*****************************************************************************/
